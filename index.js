6edb889f91b936293f2544af791572ee72d6a040
Volladapu Sowmya <99577395+Volladapu-Sowmya@users.noreply.github.com>
2023-01-02 21:58:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-const app = require("./app.js");
-const express = require("express");
-const path = require("path");
-
-app.set("view engine", "ejs");
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Started express server at port 3000");
+const app = require("./app");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server listening at port - ${port}`);
 });
